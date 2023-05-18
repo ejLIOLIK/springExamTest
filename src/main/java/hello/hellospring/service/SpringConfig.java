1e42178887caebc206dd62fcc90e05cf5f3fa576
ejLIOLIK <smile252728@gmail.com>
2023-05-18 17:57:17 +0900

    jdbc db connection (add git ignore)

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,21 +1,34 @@
 package hello.hellospring.service;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 
 //컴포넌트 스캔 아닌 자바코드로 스프링빈 설정
 @Configuration
 public class SpringConfig {
 
+	private DataSource dataSource;
+
+	@Autowired
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}	
+	
 	@Bean
 	public MemberService memberService() {
 		return new MemberService(memberRepository());
 	}
 	
 	@Bean MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
-	}	
+		//구현체만 바꿔 끼워서 DB 연동하기
+		//return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
+	}
+
 }
